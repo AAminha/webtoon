89f21eb8e45cbcff59def29f2b978e379885fb2d
AAminha <dks4857@gmail.com>
2021-08-18 01:03:13 +0900

    3.0

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,12 +5,12 @@ const Home = () => {
     const [webtoons, setWebtoons] = useState([]);
     setWebtoons = null;
 
-    /*getWebtoons = async () => {
-        const apiUrl = "dummy/webtoon.json";
+    getWebtoons = async () => {
+        const apiUrl = "https://raw.githubusercontent.com/AAminha/webtoon/master/public/dummy/webtoon.json";
 
         const webtoon = await axios.get(apiUrl);
         console.log(webtoon);
-    };*/
+    };
 };
 
 export default Home;
